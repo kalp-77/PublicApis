45f305b198b6cc6366432305c34045bf390af719
kalp patel <90538362+kalp-77@users.noreply.github.com>
2023-02-06 11:25:53 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -25,5 +25,4 @@ def index():
     return json.loads(res), 200
 
 if __name__ == "__main__":
-    app.run(debug=False)
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=False,host="0.0.0.0")
