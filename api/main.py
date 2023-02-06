befc8f765d2266872cd47c8955f8c895bc1f2e5b
kalp77 <kalp07patel@gmail.com>
2023-02-06 11:44:06 +0530

    minor change

--- api/main.py
+++ api/main.py
@@ -4,18 +4,20 @@ import json
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-PASSWORD= os.getenv("PASSWORD")
-DB_NAME= os.getenv("DB_NAME")
-COLLECTION_NAME= os.getenv("COLLECTION_NAME")
-HOST= os.getenv("HOST")
-
 app = Flask(__name__)
+
+
+load_dotenv()
+PASSWORD = os.getenv("PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME")
+HOST = os.getenv("HOST")
 
 uri = "mongodb+srv://{}:{}@{}".format("mrunmayi", PASSWORD, HOST)
 client = pymongo.MongoClient(uri)
 db = client[DB_NAME]
 collection = db[COLLECTION_NAME]
+
 
 @app.route("/")
 def index():
@@ -24,5 +26,6 @@ def index():
     res = json.dumps(data, default=str)
     return json.loads(res), 200
 
+
 if __name__ == "__main__":
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=False, host='0.0.0.0')
